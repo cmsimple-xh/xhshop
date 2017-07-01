cmsimple-xh/xhshop

commit f68e719bc465934d62cb431ea84a2e1ce8d9285b

Fix bug where orders from unsupported countries are possible
During the order process, the address of the customer is requested, and
all supported countries are offered via a select element. However, this
input is not validated on the server, so orders to arbitrary countries
could be made.

See also https://github.com/cmsimple-xh/xhshop/issues/4#issuecomment-312436466.

classes/app/xhs_frontend_controller.php
@@ -287,6 +287,18 @@ class XHS_Frontend_Controller extends XHS_Controller {
         {
             $missingData[] = 'payment_mode';
         }
+        $countries = array();
+        if(file_exists(XHS_COUNTRIES_FILE)){
+            $temp = file(XHS_COUNTRIES_FILE);
+            foreach($temp as $country){
+                if(trim($country) !== ''){
+                    $countries[] = $country;
+                }
+            }
+        }
+        if (!in_array($_SESSION['xhsCustomer']->country, $countries, true)) {
+            $missingData[] = 'country';
+        }
         if (count($missingData) > 0)
         {
             return $this->customersData($missingData);

commit 3725ebd08707f25d99a9654e9cd02a9a4a38e617

Fix regression where nothing could be ordered
f68e719 introduced a regression which prevented ever reaching the third
step of the order procedure ("Final Check"). Of course, we must
`trim()` the countries we've loaded with `file()`.

classes/app/xhs_frontend_controller.php
@@ -291,7 +291,7 @@ class XHS_Frontend_Controller extends XHS_Controller {
         if(file_exists(XHS_COUNTRIES_FILE)){
             $temp = file(XHS_COUNTRIES_FILE);
             foreach($temp as $country){
-                if(trim($country) !== ''){
+                if(($country = trim($country)) !== ''){
                     $countries[] = $country;
                 }
             }