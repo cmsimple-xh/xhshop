6c7b5493bede1c48e6687913aa90f9c1d903dad3
Christoph M. Becker <cmbecker69@gmx.de>
2017-07-09 20:23:49 +0200

    Remove unused constants

--- classes/required_classes.php
+++ classes/required_classes.php
@@ -3,12 +3,10 @@ define('XHS_LANGUAGE', basename($sl));
 define('XHS_BASE_PATH', $pth['folder']['plugins'] . 'xhshop/');
 define('XHS_LIB_PATH', XHS_BASE_PATH . 'classes/app/' );
 define('XHS_CATALOG', XHS_BASE_PATH . 'data/catalog.php' );
-define('XHS_JS_PATH', XHS_BASE_PATH .'js/' );
 
 define('XHS_COUNTRIES_FILE', XHS_BASE_PATH . 'lang/countries_' . XHS_LANGUAGE . '.txt');
 define('XHS_BILLS_PATH', XHS_BASE_PATH . 'bills/');
 define('XHS_TEMPLATES_PATH', XHS_BASE_PATH . 'templates/');
-define('XHS_CSS_PATH', XHS_BASE_PATH .  'css/');
 define('XHS_URI_SEPARATOR', $cf['uri']['seperator']);
 
 loadXHSClasses();
