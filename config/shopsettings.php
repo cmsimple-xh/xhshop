2de4e532591a34e2da2124c88f59fced63052fac
Christoph M. Becker <cmbecker69@gmx.de>
2017-07-09 17:55:30 +0200

    Remove unused settings

--- config/shopsettings.php
+++ config/shopsettings.php
@@ -1,13 +1,6 @@
 <?php
-$zShopSettings['de']['shopName'] = '';
-$zShopSettings['en']['shopName'] = '';
-$zShopSettings['product_list_chunk'] = '0';
 $zShopSettings['use_categories'] = 'true';
 $zShopSettings['allow_show_all'] = 'true';
-$zShopSettings['show_netto'] = 'false';
-$zShopSettings['vat_area']['1'] = '';
-$zShopSettings['vat_area']['2'] = '';
-$zShopSettings['vat_area']['3'] = '';
 $zShopSettings['shipping_by_unit'] = 'true';
 $zShopSettings['shipping_up_to'] = 'true';
 ?>
