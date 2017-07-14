c14ab968d6126f5b52572f382a6e1dd76a71a94e
Christoph M. Becker <cmbecker69@gmx.de>
2017-07-14 18:27:59 +0200

    Remove useless code
    `$plugin` and `$admin` are already initialized sinces ages, and
    `$hints` has been removed as of CMSimple_XH 1.6.

--- admin.php
+++ admin.php
@@ -5,15 +5,9 @@ use Xhshop\BackEndController;
 XH_registerStandardPluginMenuItems(false);
 
 if (function_exists('XH_wantsPluginAdministration') && XH_wantsPluginAdministration('xhshop') || isset($xhshop)) {
-    $plugin = basename(dirname(__FILE__), "/");
-    $admin = isset($_GET['admin']) ? $_GET['admin'] : '';
-    $admin .= isset($_POST['admin']) ? $_POST['admin'] : '';
     setcookie('xhsMode', 'edit');
 
-    if ($admin == 'plugin_stylesheet') {
-        $hint = array();
-        $o .= plugin_admin_common($action, $admin, $plugin, $hint);
-    } elseif (in_array($admin, array('plugin_config', 'plugin_language'), true)) {
+    if (in_array($admin, array('plugin_stylesheet', 'plugin_config', 'plugin_language'), true)) {
         $o .= plugin_admin_common($action, $admin, $plugin);
     } else {
         $xhsController = new BackEndController();
