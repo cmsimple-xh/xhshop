022461085e06f4fcfa3140e99f1a8903d672db01
Christoph M. Becker <cmbecker69@gmx.de>
2017-07-15 17:42:33 +0200

    Replace bill.rtf with bill.csv
    RTF has several issues so we replace it with CSV. Actually, we do not
    necessarily intend to replace RTF altogether, but rather consider to
    offer both alternatives. Until a decision in this regard has been made,
    this commit serves as POC for the CSV solution.
    
    There is `templates/frontend/confirmation_email/billtemplate.csv` which
    can be used as is or customized by the webmaster. Unnecessary
    placeholders can simply be removed, and everything can be reordered to
    fit individual needs. The only specialty is the order line row, which
    always has to be a single row, and must not contain other placeholders
    then the default ones. However, reordering and/or removing these
    placeholders is allowed.
    
    We're currently hard-coding the separator character as `;`.
    
    Whether the generated `bill.csv` has a BOM depends on whether the
    template has one.

---
+++ classes/CsvBillWriter.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Xhshop;
+
+class CsvBillWriter
+{
+    const BOM = "\xEF\xBB\xBF";
+
+    private $records = array();
+
+    private $rowRecordNum;
+
+    private $hasBom;
+
+    public function loadTemplate($template)
+    {
+        if (!($stream = fopen($template, 'r'))) {
+            return false;
+        }
+        $this->hasBom = fread($stream, 3) === self::BOM;
+        if (!$this->hasBom) {
+            rewind($stream);
+        }
+        while (($record = fgetcsv($stream, 0, ';')) !== false) {
+            $this->records[] = $record;
+        }
+        fclose($stream);
+        return true;
+    }
+
+    public function replace(array $replacements)
+    {
+        unset($this->records[$this->rowRecordNum]);
+        foreach ($replacements as $search => $replace) {
+            $cleaned = html_entity_decode($replace, ENT_QUOTES, 'UTF-8');
+            foreach ($this->records as &$record) {
+                foreach ($record as &$field) {
+                    $field = str_replace($search, $cleaned, $field);
+                }
+                unset($field);
+            }
+            unset($record);
+        }
+        $stream = fopen('php://memory', 'w+');
+        if ($this->hasBom) {
+            fwrite($stream, self::BOM);
+        }
+        foreach ($this->records as $record) {
+            fputcsv($stream, $record, ';');
+        }
+        rewind($stream);
+        $result = stream_get_contents($stream);
+        fclose($stream);
+        return $result;
+    }
+
+    public function writeProductRow($name, $amount, $price, $sum, $vatRate)
+    {
+        if (!isset($this->rowRecordNum)) {
+            foreach ($this->records as $i => $record) {
+                foreach ($record as $fields) {
+                    if (strpos($fields, '%pname%') !== false) {
+                        $this->rowRecordNum = $i;
+                        break 2;
+                    }
+                }
+            }
+        }
+        $record = $this->records[$this->rowRecordNum];
+        foreach ($record as &$field) {
+            $field = str_replace(
+                array('%pa%', '%pname%', '%pvat%', '%pprice%', '%psum%'),
+                array($amount, $name, $vatRate, $price, $sum),
+                $field
+            );
+        }
+        unset($field);
+        array_splice($this->records, $this->rowRecordNum++, 0, array($record));
+        return '';
+    }
+}
