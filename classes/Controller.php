cmsimple-xh/xhshop

commit feb2924d17d175ca2fe41a08eee135edec952ff4

Fix #104: No protection against CSRF attacks
We add CSRF protection for all relevant front-end forms, too, in this
case with our own `CSRFProction` instance. In addition, we rid the
session name and ID exposure which likely was meant to prevent CSRF,
what wouldn't have been the best idea anyway, see
<https://stackoverflow.com/questions/6968074/why-not-use-session-id-as-xsrf-token>.

classes/Controller.php
@@ -14,9 +14,14 @@ abstract class Controller
     protected $bridge;
     private $errors = array(); // apparently unused
 
+    /**
+     * @var CsrfProtection
+     */
+    protected $csrfProtector;
+
     public function __construct()
     {
-        global $pth, $plugin_cf, $plugin_tx;
+        global $pth, $plugin_cf, $plugin_tx, $_XH_csrfProtection;
 
         $this->settings = array();
         foreach ($plugin_cf['xhshop'] as $key => $value) {
@@ -50,6 +55,10 @@ abstract class Controller
         }
         $this->bridge = new CmsBridge();
         $this->catalog = new Catalogue(XHS_URI_SEPARATOR, $this->version);
+        if (!class_exists('XH_CSRFProtection')) {
+            include_once "{$pth['folder']['classes']}CsrfProtection.php";
+        }
+        $this->csrfProtector = isset($_XH_csrfProtection) ? $_XH_csrfProtection : new XH_CSRFProtection('xhs_csrf_token');
 
         $viewProvider = preg_replace('/Controller$/', 'View', get_class($this));
         $this->viewProvider = new $viewProvider();

classes/BackEndController.php
@@ -4,19 +4,6 @@ namespace Xhshop;
 
 class BackEndController extends Controller
 {
-    /**
-     * @var CsrfProtection
-     */
-    private $csrfProtector;
-
-    public function __construct()
-    {
-        global $_XH_csrfProtection;
-
-        parent::__construct();
-        $this->csrfProtector = $_XH_csrfProtection;
-    }
-
     public function handleRequest($request = null)
     {
         global $su;

classes/FrontEndController.php
@@ -55,11 +55,14 @@ class FrontEndController extends Controller
         if ($product->hasVariants()) {
             $params['variants'] = $product->getVariants(XHS_LANGUAGE);
         }
+        $params['csrf_token_input'] = $this->csrfProtector->tokenInput();
+        $this->csrfProtector->store();
         return $this->render('addToCartButton', $params);
     }
 
     public function updateCart()
     {
+        $this->csrfProtector->check();
         $variant = null;
 
         if ($this->catalog->products[$_POST['cartItem']]->hasVariants()) {
@@ -124,6 +127,8 @@ class FrontEndController extends Controller
             $params['xhs_url']   = $this->bridge->translateUrl(XHS_URL);
             $params['cartItems'] = $cartItems;
             $params['cartSum']   = $_SESSION['xhsOrder']->cartGross;
+            $params['csrf_token_input'] = $this->csrfProtector->tokenInput();
+            $this->csrfProtector->store();
             return $this->render('cartPreview', $params);
         }
         return false;
@@ -191,6 +196,7 @@ class FrontEndController extends Controller
 
     private function cart()
     {
+        $this->csrfProtector->check();
         $cartItems = $this->collectCartItems();
         if (!$cartItems) {
             return $this->productList();
@@ -215,6 +221,8 @@ class FrontEndController extends Controller
             $params['minimum_order']    = $this->settings['minimum_order'];
             $params['no_shipping_from'] = $this->settings['forwarding_expenses_up_to'];
             $params['canOrder']         = (float) $this->settings['minimum_order'] <= $_SESSION['xhsOrder']->getTotal();
+            $params['csrf_token_input'] = $this->csrfProtector->tokenInput();
+            $this->csrfProtector->store();
 
             return $this->render('cart', $params);
         }
@@ -242,12 +250,15 @@ class FrontEndController extends Controller
         $params['missingData'] = $missingData;
 
         $params['cosUrl'] = ($this->settings['cos_page']);
+        $params['csrf_token_input'] = $this->csrfProtector->tokenInput();
+        $this->csrfProtector->store();
 
         return $this->render('customersData', $params);
     }
 
     private function checkCustomersData()
     {
+        $this->csrfProtector->check();
         $missingData = array();
         $postArray = array();
         foreach ($_POST as $key => $value) {
@@ -373,6 +384,8 @@ class FrontEndController extends Controller
             $params['fullRate']    = $this->settings['vat_full'];
             $params['reducedRate'] = $this->settings['vat_reduced'];
         }
+        $params['csrf_token_input'] = $this->csrfProtector->tokenInput();
+        $this->csrfProtector->store();
 
         return $this->render('finalConfirmation', $params);
     }
@@ -383,6 +396,7 @@ class FrontEndController extends Controller
      */
     private function finishCheckOut()
     {
+        $this->csrfProtector->check();
         if (!isset($_SESSION['xhsCustomer']) || !isset($_SESSION['xhsOrder'])) {
             return '';
         }

templates/frontend/addToCartButton.tpl
@@ -9,9 +9,9 @@
 			<br></span>
 		</div>
 		<form method="post">
+			%CSRF_TOKEN_INPUT%
 			<input type="hidden" name="xhsTask" value="updateCart">
 			<input type="hidden" name="cartItem" value="<?php echo $this->product->uid ; ?>">
-			<input type="hidden" name="<?php echo session_name(); ?>" value="<?php echo session_id(); ?>">
 			<?php if(isset($this->variants)){ ?>
 			<select name="xhsVariant">
 				<?php  foreach($this->variants as $index => $variant){ ?>

templates/frontend/cart.tpl
@@ -28,6 +28,7 @@ foreach($this->cartItems as $product){
 <td>&nbsp;</td>
 <td class="xhsMoneyCell">
 <form method="post" class="xhsInl">
+%CSRF_TOKEN_INPUT%
 <!--<input class="xhsInpAmount" type="number" min="1" step="1" name="xhsAmount" value="<?php echo $product['amount']; ?>" onChange="this.parentNode.submit()">-->
 <input class="xhsInpAmount" type="number" min="1" step="1" name="xhsAmount" value="<?php echo $product['amount']; ?>"> x <?php echo $this->formatCurrency($product['price']) ;?><br>
 <input type="hidden" name="xhsTask" value="updateCart">
@@ -36,6 +37,7 @@ foreach($this->cartItems as $product){
 <input type="hidden" name="cartItem" value="<?php echo $product['key'];?>">
 <button class="xhsUpdBtn" title="<?php $this->label('update'); ?>"><span class="fa fa-refresh fa-lg"></span></button>
 </form> <form method="post" class="xhsInl">
+%CSRF_TOKEN_INPUT%
 <input type="hidden" name="xhsAmount" value="0" />
 <input type="hidden" name="xhsTask" value="updateCart" />
 <input type="hidden" name="xhsCheckout" value="cart" />

templates/frontend/cartPreview.tpl
@@ -2,8 +2,8 @@
 <div id="xhsCartPreview">
 <?php foreach($this->cartItems as $product){}?>
 	<form action="%XHS_URL%" method="post">
+		%CSRF_TOKEN_INPUT%
 		<button class="xhsShopButton xhsCrt" title="<?php echo $product['itemCounter']?> <?php echo $this->label('products'); ?> ⇒ <?php $this->label('go_to_cart'); ?>"><span class="fa fa-shopping-cart fa-2x"></span><span class="xhsItemCounter xhsBadge"><?php echo $product['itemCounter']; ?></span></button>
-		<input type="hidden" name="<?php echo session_name(); ?>" value="<?php echo session_id(); ?>">
 		<input type = "hidden" name= "xhsCheckout" value="cart">
 	</form>
 </div>

templates/frontend/catalog.tpl
@@ -59,6 +59,7 @@
 				x
 				<input type="hidden" name="cartItem" value="<?php echo $index; ?>" />
 				<button class="xhsShopButton xhsAddCart"><span class="fa fa-cart-plus fa-lg fa-fw"></span> <?php echo $this->label('add_to_cart'); ?></button>
+				%CSRF_TOKEN_INPUT%
 			</form>
 		</div>
 	</article>

templates/frontend/customersData.tpl
@@ -53,6 +53,7 @@ if(in_array('cos_confirmed', $this->missingData)){
 <!-- End annotation -->
 <p> </p>
 <input type="hidden" name="xhsCheckout" value="checkCustomersData">
+%CSRF_TOKEN_INPUT%
 <button class="xhsShopButton xhsRght xhsInl"><?php $this->label('next'); ?> <span class="fa fa-arrow-circle-right fa-fw"></span></button>
 </form>
 <form method="post" class="xhsLft">

commit 4a53247a84904d7a12878b32dbd16c0fe0441907

Fix another regression introduced in feb2924

classes/Controller.php
@@ -2,6 +2,8 @@
 
 namespace Xhshop;
 
+use XH_CSRFProtection;
+
 abstract class Controller
 {
     protected $viewProvider;