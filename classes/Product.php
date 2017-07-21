cmsimple-xh/xhshop

commit 0f1cf4c6899a79603a2cb8df41801f1af66db350

Encapsulate `Product`

classes/Product.php
@@ -4,23 +4,56 @@ namespace Xhshop;
 
 class Product
 {
-    public $names;
-    public $descriptions;
-    public $teasers;
-    public $price;
-    public $productPages;
-    public $previewPicture;
-    public $vat;
-    public $separator;
-    public $categories;
-    public $stock_on_hand;
-    public $weight;
-    public $variants;
-    public $uid;
-    public $sortIndex;
-    public $image;
+    private $names;
+    private $descriptions;
+    private $teasers;
+    private $price;
+    private $productPages;
+    private $previewPicture;
+    private $vat;
+    private $separator;
+    private $categories;
+    private $stock_on_hand;
+    private $weight;
+    private $variants;
+    private $uid;
+    private $sortIndex;
+    private $image;
     private $imageFolder;
     private $previewFolder;
+
+    public static function createFromRecord(array $record, $index, $separator)
+    {
+        $result = new self;
+        $result->names = $record['names'];
+        $result->price = $record['price'];
+        $result->vat = $record['vat'];
+        $result->variants = isset($record['variants']) ? $record['variants'] : array(XHS_LANGUAGE => '');
+        $result->previewPicture = isset($record['previewPicture']) ? $record['previewPicture'] : '';
+        $result->image = isset($record['image']) ? $record['image'] : '';
+        $result->weight = $record['weight'];
+        $result->setStockOnHand(isset($record['stock_on_hand']) ? $record['stock_on_hand'] : 1);
+        $result->teasers = isset($record['teasers']) ? $record['teasers'] : array(XHS_LANGUAGE => '');
+        $result->descriptions = isset($record['descriptions']) ? $record['descriptions'] :array(XHS_LANGUAGE => '');
+        $result->categories = isset($record['categories']) ? $record['categories'] : array(XHS_LANGUAGE => '');
+        $result->productPages = isset($record['productPages'])
+            ? $record['productPages']
+            : array(XHS_LANGUAGE => array());
+
+        if ($record['separator'] != $separator) {
+            $new_links = array();
+            foreach ($temp['productPages'][XHS_LANGUAGE] as $page) {
+                $new_links[] = str_replace($record['separator'], $separator, $page);
+            }
+            $result->productPages[XHS_LANGUAGE] = $new_links;
+        }
+
+
+        $result->sortIndex = isset($record['sortIndex']) ? $record['sortIndex'] : $index;
+        $result->uid = isset($record['uid']) ? $record['uid'] : uniqid('p');
+        $result->separator = $separator;
+        return $result;
+    }
 
     public function __construct()
     {
@@ -35,6 +68,24 @@ class Product
         $this->uid = uniqid('p');
         $this->imageFolder = "{$pth['folder']['images']}{$plugin_cf['xhshop']['shop_image_folder']}";
         $this->previewFolder = "{$pth['folder']['images']}{$plugin_cf['xhshop']['shop_preview_folder']}";
+    }
+
+    /**
+     * For serialization only!
+     */
+    public function getInternalState()
+    {
+        return (object) get_object_vars($this);
+    }
+
+    public function getUid()
+    {
+        return $this->uid;
+    }
+
+    public function getSortIndex()
+    {
+        return $this->sortIndex;
     }
 
     public function getWeight()
@@ -120,6 +171,11 @@ class Product
         return '';
     }
 
+    public function getVat()
+    {
+        return $this->vat;
+    }
+
     public function getImageName()
     {
         if (isset($this->image)) {
@@ -159,6 +215,16 @@ class Product
         return (isset($this->variants[XHS_LANGUAGE]) && is_array($this->variants[XHS_LANGUAGE]))
             ? count($this->variants[XHS_LANGUAGE]) > 0
             : null;
+    }
+
+    public function setSortIndex($value)
+    {
+        $this->sortIndex = $value;
+    }
+
+    public function setSeparator($value)
+    {
+        $this->separator = $value;
     }
 
     public function setName($name = 'No Name!', $language = XHS_LANGUAGE)
@@ -220,6 +286,11 @@ class Product
     public function setCategories(array $categories = array(), $language = XHS_LANGUAGE)
     {
         $this->categories[$language] = $categories;
+    }
+
+    public function addCategory($key, $value)
+    {
+        $this->categories[XHS_LANGUAGE][$key] = $value;
     }
 
     public function setPreviewPic($pic = '')

classes/BackEndController.php
@@ -99,12 +99,12 @@ class BackEndController extends Controller
                 'xhsImage'
             );
             $params['variants']    = $product->hasVariants() ? implode('; ', $product->getVariants(XHS_LANGUAGE)) : '';
-            $params['name']        = $product->names[XHS_LANGUAGE];
+            $params['name']        = $product->getName();
             $params['teaser']      = $product->getTeaser(XHS_LANGUAGE);
             $params['description'] = $product->getDescription(XHS_LANGUAGE);
             $params['price']       = $product->getGross();
             $params['weight']      = $product->getWeight();
-            $params['stockOnHand'] = is_int($product->stock_on_hand) ? $product->stock_on_hand : 1;
+            $params['stockOnHand'] = $product->isAvailable();
             $params['preview']     = $this->viewProvider->linkedImage(
                 $product->getPreviewPicturePath(),
                 $product->getPreviewPicturePath(),
@@ -115,7 +115,7 @@ class BackEndController extends Controller
                 $product->getImagePath(),
                 $product->getName(XHS_LANGUAGE)
             );
-            $params['vat']         = $product->vat;
+            $params['vat']         = $product->getVat();
             $params['pages']       = $product->getProductPages();
             $params['productCats'] = $product->getCategories();
         } else {

classes/CatalogWriter.php
@@ -2,11 +2,13 @@
 
 namespace Xhshop;
 
+use stdClass;
+
 class CatalogWriter
 {
     private $catalog;
 
-    public function __construct(Catalogue $catalog)
+    public function __construct(stdClass $catalog)
     {
         $this->catalog = $catalog;
     }
@@ -16,9 +18,9 @@ class CatalogWriter
         $string = "<?php \n";
         //      $string .= '$separator = \'' . $this->separator . "';\n\n";
         $string .= '################### Catalog ###############################' . ";\n";
-        $string .= '$version = \'' . $this->catalog->getVersion() . "';\n\n";
+        $string .= '$version = \'' . $this->catalog->version . "';\n\n";
         $string .= '################### Categories ###############################' . ";\n";
-        foreach ($this->catalog->getAllCategories() as $lang => $categories) {
+        foreach ($this->catalog->categories as $lang => $categories) {
             if (!is_array($categories)) {
                 $string .= '$categories[\'' . $lang . '\'] = array();' . ";\n";
             } else {
@@ -28,14 +30,15 @@ class CatalogWriter
                 }
             }
         }
-        foreach ($this->catalog->getAllDefaultCategories() as $lang => $cat) {
+        foreach ($this->catalog->default_category as $lang => $cat) {
             $string .= '$default_category[\'' . $lang . '\'] = \'' . $this->cleanString($cat) . "';\n";
         }
-        foreach ($this->catalog->getAllLeftOverCategories() as $lang => $cat) {
+        foreach ($this->catalog->category_for_the_left_overs as $lang => $cat) {
             $string .= '$category_for_the_left_overs[\'' . $lang . '\'] = \'' . $this->cleanString($cat) . "';\n";
         }
         $string .= "\n\n" . '################### Products ######################' . ";\n";
-        foreach ($this->catalog->getProducts() as $uid => $product) {
+        foreach ($this->catalog->products as $uid => $product) {
+            $product = $product->getInternalState();
             foreach ($product->names as $lang => $name) {
                 $string .= '$products[\'' . $uid . '\'][\'names\'][\'' . $lang . '\'] = \''
                     . $this->cleanString($name) . "';\n";

classes/Catalogue.php
@@ -21,11 +21,6 @@ class Catalogue
         $this->loadArray();
     }
 
-    public function getVersion()
-    {
-        return $this->version;
-    }
-
     private function loadArray()
     {
         $categories = $category_for_the_left_overs = $default_category = array();
@@ -39,37 +34,10 @@ class Catalogue
             $products = array();
         }
         $i = count($products);
-        foreach ($products as $temp) {
-            $product = new Product();
-            $product->names = $temp['names'];
-            $product->price = $temp['price'];
-            $product->vat = $temp['vat'];
-            $product->variants = isset($temp['variants']) ? $temp['variants'] : array(XHS_LANGUAGE => '');
-            $product->previewPicture = isset($temp['previewPicture']) ? $temp['previewPicture'] : '';
-            $product->image = isset($temp['image']) ? $temp['image'] : '';
-            $product->weight = $temp['weight'];
-            $product->setStockOnHand(isset($temp['stock_on_hand']) ? $temp['stock_on_hand'] : 1);
-            $product->teasers = isset($temp['teasers']) ? $temp['teasers'] : array(XHS_LANGUAGE => '');
-            $product->descriptions = isset($temp['descriptions']) ? $temp['descriptions'] :array(XHS_LANGUAGE => '');
-            $product->categories = isset($temp['categories']) ? $temp['categories'] : array(XHS_LANGUAGE => '');
-            $product->productPages = isset($temp['productPages'])
-                ? $temp['productPages']
-                : array(XHS_LANGUAGE => array());
-
-            if ($temp['separator'] <> $this->separator) {
-                $new_links = array();
-                foreach ($temp['productPages'][XHS_LANGUAGE] as $page) {
-                    $new_links[] = str_replace($temp['separator'], $this->separator, $page);
-                }
-                $product->productPages[XHS_LANGUAGE] = $new_links;
-            }
-
-
-            $product->sortIndex = isset($temp['sortIndex']) ? $temp['sortIndex'] : $i;
+        foreach ($products as $record) {
+            $product = Product::createFromRecord($record, $i, $this->separator);
+            $this->products[$product->getUid()] = $product;
             $i--;
-            $product->uid = isset($temp['uid']) ? $temp['uid'] :uniqid('p');
-            $product->separator = $this->separator;
-            $this->products[$product->uid] = $product;
         }
     }
 
@@ -80,12 +48,9 @@ class Catalogue
 
         foreach ($this->products as $product) {
             if ($product instanceof Product) {
-                $product->separator = $this->separator;
-                if (!isset($product->uid)) {
-                    $product->uid = uniqid('p');
-                }
-                $products[$product->uid] = $product;
-                $sortOrder[$product->uid] = $product->sortIndex;
+                $product->setSeparator($this->separator);
+                $products[$product->getUid()] = $product;
+                $sortOrder[$product->getUid()] = $product->getSortIndex();
             }
         }
 
@@ -94,13 +59,13 @@ class Catalogue
 
         $i = 1;
         foreach (array_keys($sortOrder) as $key) {
-            $products[$key]->sortIndex = $i;
+            $products[$key]->setSortIndex($i);
             $i++;
         }
 
         $this->products = isset($products) ? $products : array();
 
-        $writer = new CatalogWriter($this);
+        $writer = new CatalogWriter((object) get_object_vars($this));
         $writer->write();
         $this->loadArray();
         return;
@@ -113,9 +78,9 @@ class Catalogue
         }
         $products = $this->getProducts($this->categories[XHS_LANGUAGE][$index]);
         foreach ($products as $product) {
-            foreach ($product->categories[XHS_LANGUAGE] as $key => $value) {
+            foreach ($product->getCategories() as $key => $value) {
                 if ($value == $this->categories[XHS_LANGUAGE][$index]) {
-                    $product->categories[XHS_LANGUAGE][$key] = $name;
+                    $product->addCategory($key, $name);
                 }
             }
         }
@@ -158,7 +123,7 @@ class Catalogue
     public function hasUncategorizedProducts()
     {
         foreach ($this->products as $product) {
-            if (!isset($product->categories[XHS_LANGUAGE]) || count($product->categories[XHS_LANGUAGE]) == 0) {
+            if (count($product->getCategories()) == 0) {
                 return true;
             }
         }
@@ -169,7 +134,7 @@ class Catalogue
     {
         $products = array();
         foreach ($this->products as $index => $product) {
-            if (!isset($product->categories[XHS_LANGUAGE]) || !$product->categories[XHS_LANGUAGE]) {
+            if (count($product->getCategories()) > 0) {
                 $products[$index] = $product;
             }
         }
@@ -187,9 +152,7 @@ class Catalogue
             if (in_array($category, $this->categories[XHS_LANGUAGE])) {
                 $products = array();
                 foreach ($this->products as $index => $product) {
-                    if (isset($product->categories[XHS_LANGUAGE])
-                        && is_array($product->categories[XHS_LANGUAGE])
-                        && in_array($category, $product->categories[XHS_LANGUAGE])) {
+                    if (in_array($category, $product->getCategories())) {
                         $products[$index] = $product;
                     }
                 }
@@ -204,10 +167,10 @@ class Catalogue
     }
     public function swapSortIndex(Product $productA, Product $productB)
     {
-        $swap = $productA->sortIndex;
+        $swap = $productA->getSortIndex();
 
-        $productA->sortIndex = $productB->sortIndex;
-        $productB->sortIndex = $swap;
+        $productA->setSortIndex($productB->getSortIndex());
+        $productB->setSortIndex($swap);
         $this->save();
     }
 
@@ -223,7 +186,7 @@ class Catalogue
 
     public function getLastProductId()
     {
-        return end($this->catalog->products)->uid;
+        return end($this->products)->getUid();
     }
 
     public function getCategories($language = XHS_LANGUAGE)
@@ -233,21 +196,6 @@ class Catalogue
             $this->save();
         }
         return $this->categories[$language];
-    }
-
-    public function getAllCategories()
-    {
-        return $this->categories;
-    }
-
-    public function getAllDefaultCategories()
-    {
-        return $this->default_category;
-    }
-
-    public function getAllLeftOverCategories()
-    {
-        return $this->category_for_the_left_overs;
     }
 
     public function moveCategory($direction = null, $index = null)
@@ -282,7 +230,7 @@ class Catalogue
 
     public function addProduct(Product $product)
     {
-        $product->sortIndex = 0;
+        $product->setSortIndex(0);
         $this->products[] = $product;
         $this->save();
     }

classes/Controller.php
@@ -126,7 +126,7 @@ abstract class Controller
 
         $productList = $this->catalog->getProducts($category);
         uasort($productList, function (Product $productA, Product $productB) {
-            return $productA->sortIndex - $productB->sortIndex;
+            return $productA->getSortIndex() - $productB->getSortIndex();
         });
         $products = array();
 
@@ -149,7 +149,7 @@ abstract class Controller
             $products[$index]['teaser'] = $product->getTeaser();
             $products[$index]['detailLink'] = $detailLink;
             $products[$index]['price'] = $product->getGross();
-            $products[$index]['sortIndex'] = $product->sortIndex;
+            $products[$index]['sortIndex'] = $product->getSortIndex();
             $products[$index]['isAvailable'] = $product->isAvailable();
             $products[$index]['previewPicture'] = $this->viewProvider->linkedImage(
                 $product->getPreviewPicturePath(),
@@ -172,14 +172,11 @@ abstract class Controller
         $url = $this->bridge->getCurrentPage();
         $products = array();
         foreach ($this->catalog->getProducts() as $product) {
-            if (!isset($product->productPages[XHS_LANGUAGE])) {
-                continue;
-            }
-            if (isset($product->stock_on_hand) && $product->stock_on_hand < 1) {
+            if (!$product->isAvailable()) {
                 continue;
             }
 
-            foreach ($product->productPages[XHS_LANGUAGE] as $page) {
+            foreach ($product->getProductPages() as $page) {
                 if ($page == $this->bridge->translateUrl($url) || $page == $url) {
                     $products[] = $product;
                 }

classes/FrontEndController.php
@@ -51,7 +51,7 @@ class FrontEndController extends Controller
         $params = array('productName' => $product->getName(XHS_LANGUAGE),
             'product'     => $product,
             'vatInfo'     => $this->vatInfo(),
-            'vatRate'     => $this->settings['vat_' . $product->vat]);
+            'vatRate'     => $this->settings['vat_' . $product->getVat()]);
         if ($product->hasVariants()) {
             $params['variants'] = $product->getVariants(XHS_LANGUAGE);
         }
@@ -173,7 +173,7 @@ class FrontEndController extends Controller
                     $name       = $this->viewProvider->link($page, $name);
                     $detailLink = $this->viewProvider->link($page, $this->viewProvider->labels['product_info']);
                 }
-                $vatRate                          = 'vat_' . $this->catalog->getProduct($productKey)->vat;
+                $vatRate                          = 'vat_' . $this->catalog->getProduct($productKey)->getVat();
                 $vatRate                          = $this->settings[$vatRate];
                 $cartItems[$index]['name']        = $name;
                 $cartItems[$index]['key']         = $productKey;
@@ -185,9 +185,9 @@ class FrontEndController extends Controller
                 $cartItems[$index]['price']       = $product['gross'];
                 $cartItems[$index]['vatRate']     = $vatRate;
                 $cartItems[$index]['sum']         = $product['gross'] * $product['amount'];
-                if ($this->catalog->getProduct($productKey)->previewPicture) {
-                    $cartItems[$index]['previewPicture'] = $this->settings['image_folder']
-                        . $this->catalog->getProduct($productKey)->previewPicture;
+                if ($this->catalog->getProduct($productKey)->getPreviewPictureName()) {
+                    $cartItems[$index]['previewPicture'] =
+                            $this->catalog->getProduct($productKey)->getPreviewPicturePath();
                 }
 
                 $i++;
@@ -623,10 +623,10 @@ class FrontEndController extends Controller
         $params['description'] = $product->getDescription();
         $params['button']      = $this->addToCartButton($product);
         $params['variants']    = count($product->getVariants() > 0) ? $product->getVariants() : false;
-        $params['price']       = $product->price;
-        $params['uid']         = $product->uid;
+        $params['price']       = $product->getGross();
+        $params['uid']         = $product->getUid();
         $params['hideVat']     = (bool) $this->settings['dont_deal_with_taxes'];
-        $params['vatRate']     = $this->settings['vat_' . $product->vat];
+        $params['vatRate']     = $this->settings['vat_' . $product->getVat()];
         $params['vatInfo']     = $this->vatInfo();
         $params['image']       = $this->viewProvider->linkedImage(
             $product->getPreviewPicturePath(),

classes/Order.php
@@ -26,22 +26,22 @@ class Order
 
     public function addItem(Product $product, $amount, $variant = null)
     {
-        $index = $product->uid;
+        $index = $product->getUid();
         if (isset($variant)) {
             $index .= '_'.$variant;
         }
         $this->items[$index]['amount'] = (int)$amount;
         $this->items[$index]['variant'] = $variant;
         $this->items[$index]['net'] = (float)$this->getProductNet($product);
-        $this->items[$index]['gross'] = (float)$product->price;
-        $this->items[$index]['vatRate'] = $product->vat;
+        $this->items[$index]['gross'] = $product->getGross();
+        $this->items[$index]['vatRate'] = $product->getVat();
         $this->items[$index]['units'] = (float)$product->getWeight();
         $this->refresh();
     }
 
     public function removeItem(Product $product, $variant = null)
     {
-        $index = $product->uid;
+        $index = $product->getUid();
         if (isset($variant)) {
             $index .= '_'.$variant;
         }
@@ -52,10 +52,9 @@ class Order
     private function getProductNet(Product $product)
     {
         $rate = 0;
-        if ($product->vat == 'full') {
+        if ($product->getVat() == 'full') {
             $rate = $this->vatFullRate;
-        }
-        if ($product->vat == 'reduced') {
+        } elseif ($product->getVat() == 'reduced') {
             $rate = $this->vatReducedRate;
         }
         return $product->getNet($rate);

templates/frontend/addToCartButton.tpl
@@ -2,7 +2,7 @@
 <section id="cartButton" class="xhsMain">
 	<h1 class="xhsInl xhsLft"><?php echo $this->productName; ?></h1>
 	<div class="xhsInfoBlock">
-		<div class="xhsPrdPrice"><span class="xhsPrdPriceLabel"><?php echo $this->labels['price'];?></span> <span class="xhsPrdPriceNum"><?php echo $this->formatCurrency($this->product->price); ?></span></div>
+		<div class="xhsPrdPrice"><span class="xhsPrdPriceLabel"><?php echo $this->labels['price'];?></span> <span class="xhsPrdPriceNum"><?php echo $this->formatCurrency($this->product->getGross()); ?></span></div>
 		<div class="price_info"><span class="xhsPrdPriceLabel">
 			 <?php $this->hint($this->vatInfo); ?> (<?php echo $this->formatFloat($this->vatRate) ?> %)<br>
             <?php echo $this->shippingCostsHint(); ?>  
@@ -11,7 +11,7 @@
 		<form method="post">
 			%CSRF_TOKEN_INPUT%
 			<input type="hidden" name="xhsTask" value="updateCart">
-			<input type="hidden" name="cartItem" value="<?php echo $this->product->uid ; ?>">
+			<input type="hidden" name="cartItem" value="<?php echo $this->product->getUid() ; ?>">
 			<?php if(isset($this->variants)){ ?>
 			<select name="xhsVariant">
 				<?php  foreach($this->variants as $index => $variant){ ?>

commit c8b11f5f79cc081814f145ab09bb5aa9406f1ad2

Fix regression where automatic correction of separator was broken
In 0f1cf4c we forgot to rename `$temp`.

classes/Product.php
@@ -42,7 +42,7 @@ class Product
 
         if ($record['separator'] != $separator) {
             $new_links = array();
-            foreach ($temp['productPages'][XHS_LANGUAGE] as $page) {
+            foreach ($record['productPages'][XHS_LANGUAGE] as $page) {
                 $new_links[] = str_replace($record['separator'], $separator, $page);
             }
             $result->productPages[XHS_LANGUAGE] = $new_links;