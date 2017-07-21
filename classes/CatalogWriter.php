5d328a37b2c1710903539a85d96d2f3e226960d9
Christoph M. Becker <cmbecker69@gmx.de>
2017-07-21 10:53:52 +0200

    Refactor `CatalogWriter`

--- classes/CatalogWriter.php
+++ classes/CatalogWriter.php
@@ -15,100 +15,7 @@ class CatalogWriter
 
     public function write()
     {
-        $string = "<?php \n";
-        //      $string .= '$separator = \'' . $this->separator . "';\n\n";
-        $string .= '################### Catalog ###############################' . ";\n";
-        $string .= '$version = \'' . $this->catalog->version . "';\n\n";
-        $string .= '################### Categories ###############################' . ";\n";
-        foreach ($this->catalog->categories as $lang => $categories) {
-            if (!is_array($categories)) {
-                $string .= '$categories[\'' . $lang . '\'] = array();' . ";\n";
-            } else {
-                foreach ($categories as $key => $category) {
-                    $string .= '$categories[\'' . $lang . '\'][' . $key . '] = \''
-                        . $this->cleanString($category) . "';\n";
-                }
-            }
-        }
-        foreach ($this->catalog->default_category as $lang => $cat) {
-            $string .= '$default_category[\'' . $lang . '\'] = \'' . $this->cleanString($cat) . "';\n";
-        }
-        foreach ($this->catalog->category_for_the_left_overs as $lang => $cat) {
-            $string .= '$category_for_the_left_overs[\'' . $lang . '\'] = \'' . $this->cleanString($cat) . "';\n";
-        }
-        $string .= "\n\n" . '################### Products ######################' . ";\n";
-        foreach ($this->catalog->products as $uid => $product) {
-            $product = $product->getInternalState();
-            foreach ($product->names as $lang => $name) {
-                $string .= '$products[\'' . $uid . '\'][\'names\'][\'' . $lang . '\'] = \''
-                    . $this->cleanString($name) . "';\n";
-            }
-            $string .= '$products[\'' . $uid . '\'][\'price\'] = ' . number_format($product->price, 2, '.', '') . ";\n";
-            $string .= '$products[\'' . $uid . '\'][\'vat\'] = \'' . $this->cleanString($product->vat) . "';\n";
-            $string .= '$products[\'' . $uid . '\'][\'sortIndex\'] = ' . (int)$product->sortIndex . ";\n";
-            $string .= '$products[\'' . $uid . '\'][\'previewPicture\'] = \'' . $product->previewPicture . "';\n";
-            $string .= '$products[\'' . $uid . '\'][\'image\'] = \'' . $product->image . "';\n";
-            $string .= '$products[\'' . $uid . '\'][\'weight\'] = ' . number_format($product->weight, 2, '.', '') . ";\n";
-            $string .= '$products[\'' . $uid . '\'][\'stock_on_hand\'] = ' . (int)$product->stock_on_hand . ";\n";
-            if (!isset($product->teasers)) {
-                $product->teasers = array(XHS_LANGUAGE => '');
-            }
-            foreach ($product->teasers as $lang => $teaser) {
-                $string .= '$products[\'' . $uid . '\'][\'teasers\'][\'' . $lang . '\'] = \'' . $this->cleanString($teaser) . "';\n";
-            }
-            foreach ($product->descriptions as $lang => $description) {
-                $string .= '$products[\'' . $uid . '\'][\'descriptions\'][\'' . $lang . '\'] = \''
-                    . $this->cleanString($description) . "';\n";
-            }
-            
-            if (!isset($product->variants) || !is_array($product->variants)) {
-                $string .= '$products[\'' . $uid . '\'][\'variants\'] = array('.XHS_LANGUAGE .' => \'\')' . ";\n";
-            } else {
-                foreach ($product->variants as $lang => $variants) {
-                    $string .= '$products[\'' . $uid . '\'][\'variants\'][\'' . $lang . '\'] = array(';
-                    if (is_array($variants)) {
-                        foreach ($variants as $variant) {
-                            $string .= "'" . trim($this->cleanString($variant)) . "', ";
-                        }
-                    }
-                    $string .= ');' . "\n";
-                }
-            }
-
-            if (!isset($product->categories) || !is_array($product->categories)) {
-                $string .= '$products[\'' . $uid . '\'][\'categories\'] = array()' . ";\n";
-            } else {
-                foreach ($product->categories as $lang => $categories) {
-                    $string .= '$products[\'' . $uid . '\'][\'categories\'][\'' . $lang . '\'] = array(';
-                    if (is_array($categories)) {
-                        foreach ($categories as $cat) {
-                            $string .= "'" . $this->cleanString($cat) . "', ";
-                        }
-                    }
-                    $string .= ');' . "\n";
-                }
-            }
-
-            if (!isset($product->productPages)) {
-                $string .= '$products[\'' . $uid . '\'][\'productPages\'] = array()' . ";\n";
-            } else {
-                foreach ($product->productPages as $lang => $pages) {
-                    $string .= '$products[\'' . $uid . '\'][\'productPages\'][\'' . $lang . '\'] = array(';
-                    if (is_array($pages)) {
-                        foreach ($pages as $page) {
-                            $string .= "'" . $this->cleanString($page) . "', ";
-                        }
-                    }
-                    $string .= ');' . "\n";
-                }
-            }
-            $string .= '$products[\'' . $uid . '\'][\'separator\'] = \''
-                . $this->cleanString($product->separator) . "';\n";
-            $string .= '$products[\'' . $uid . '\'][\'uid\'] = \'' . $this->cleanString($uid) . "';\n";
-            $string .= "\n#-----------------------------------------------------\n\n";
-        }
-
-        $string .= '?>';
+        $string = $this->emitCatalog();
         if (!file_exists(XHS_CATALOG)) {
             $handle = fopen(XHS_CATALOG, 'w');
 
@@ -133,13 +40,133 @@ class CatalogWriter
         return true;
     }
 
-    private function cleanString($string, $writeEntities = false)
+    private function emitCatalog()
     {
-        $string = str_replace(array('./', '<?php', '<?', '?>'), '', $string);
-        if ($writeEntities === true) {
-            $string = htmlspecialchars($string);
+        $string = "<?php \n";
+        //      $string .= '$separator = \'' . $this->separator . "';\n\n";
+        $string .= '################### Catalog ###############################' . ";\n";
+        $string .= '$version = \'' . $this->catalog->version . "';\n\n";
+        $string .= '################### Categories ###############################' . ";\n";
+        foreach ($this->catalog->categories as $lang => $categories) {
+            if (!is_array($categories)) {
+                $string .= '$categories[\'' . $lang . '\'] = array();' . ";\n";
+            } else {
+                foreach ($categories as $key => $category) {
+                    $string .= '$categories[\'' . $lang . '\'][' . $key . '] = \''
+                        . $this->cleanString($category) . "';\n";
+                }
+            }
+        }
+        foreach ($this->catalog->default_category as $lang => $cat) {
+            $string .= '$default_category[\'' . $lang . '\'] = \'' . $this->cleanString($cat) . "';\n";
+        }
+        foreach ($this->catalog->category_for_the_left_overs as $lang => $cat) {
+            $string .= '$category_for_the_left_overs[\'' . $lang . '\'] = \'' . $this->cleanString($cat) . "';\n";
+        }
+        $string .= "\n\n" . '################### Products ######################' . ";\n";
+        foreach ($this->catalog->products as $uid => $product) {
+            $string .= $this->emitProduct($uid, $product->getInternalState());
         }
 
+        $string .= '?>';
+        return $string;
+    }
+
+    private function emitProduct($uid, stdClass $product)
+    {
+        $string = '';
+        foreach ($product->names as $lang => $name) {
+            $string .= '$products[\'' . $uid . '\'][\'names\'][\'' . $lang . '\'] = \''
+                . $this->cleanString($name) . "';\n";
+        }
+        $string .= '$products[\'' . $uid . '\'][\'price\'] = ' . number_format($product->price, 2, '.', '') . ";\n";
+        $string .= '$products[\'' . $uid . '\'][\'vat\'] = \'' . $this->cleanString($product->vat) . "';\n";
+        $string .= '$products[\'' . $uid . '\'][\'sortIndex\'] = ' . (int)$product->sortIndex . ";\n";
+        $string .= '$products[\'' . $uid . '\'][\'previewPicture\'] = \'' . $product->previewPicture . "';\n";
+        $string .= '$products[\'' . $uid . '\'][\'image\'] = \'' . $product->image . "';\n";
+        $string .= '$products[\'' . $uid . '\'][\'weight\'] = ' . number_format($product->weight, 2, '.', '') . ";\n";
+        $string .= '$products[\'' . $uid . '\'][\'stock_on_hand\'] = ' . (int)$product->stock_on_hand . ";\n";
+        if (!isset($product->teasers)) {
+            $product->teasers = array(XHS_LANGUAGE => '');
+        }
+        foreach ($product->teasers as $lang => $teaser) {
+            $string .= '$products[\'' . $uid . '\'][\'teasers\'][\'' . $lang . '\'] = \'' . $this->cleanString($teaser) . "';\n";
+        }
+        foreach ($product->descriptions as $lang => $description) {
+            $string .= '$products[\'' . $uid . '\'][\'descriptions\'][\'' . $lang . '\'] = \''
+                . $this->cleanString($description) . "';\n";
+        }
+        
+        $string .= $this->emitVariants($uid, $product);
+        $string .= $this->emitCategories($uid, $product);
+        $string .= $this->emitProductPages($uid, $product);
+        $string .= '$products[\'' . $uid . '\'][\'separator\'] = \''
+            . $this->cleanString($product->separator) . "';\n";
+        $string .= '$products[\'' . $uid . '\'][\'uid\'] = \'' . $this->cleanString($uid) . "';\n";
+        $string .= "\n#-----------------------------------------------------\n\n";
+        return $string;
+    }
+
+    private function emitVariants($uid, stdClass $product)
+    {
+        $string = '';
+        if (!isset($product->variants) || !is_array($product->variants)) {
+            $string .= '$products[\'' . $uid . '\'][\'variants\'] = array('.XHS_LANGUAGE .' => \'\')' . ";\n";
+        } else {
+            foreach ($product->variants as $lang => $variants) {
+                $string .= '$products[\'' . $uid . '\'][\'variants\'][\'' . $lang . '\'] = array(';
+                if (is_array($variants)) {
+                    foreach ($variants as $variant) {
+                        $string .= "'" . trim($this->cleanString($variant)) . "', ";
+                    }
+                }
+                $string .= ');' . "\n";
+            }
+        }
+        return $string;
+    }
+
+    private function emitCategories($uid, stdClass $product)
+    {
+        $string = '';
+        if (!isset($product->categories) || !is_array($product->categories)) {
+            $string .= '$products[\'' . $uid . '\'][\'categories\'] = array()' . ";\n";
+        } else {
+            foreach ($product->categories as $lang => $categories) {
+                $string .= '$products[\'' . $uid . '\'][\'categories\'][\'' . $lang . '\'] = array(';
+                if (is_array($categories)) {
+                    foreach ($categories as $cat) {
+                        $string .= "'" . $this->cleanString($cat) . "', ";
+                    }
+                }
+                $string .= ');' . "\n";
+            }
+        }
+        return $string;
+    }
+
+    private function emitProductPages($uid, stdClass $product)
+    {
+        $string = '';
+        if (!isset($product->productPages)) {
+            $string .= '$products[\'' . $uid . '\'][\'productPages\'] = array()' . ";\n";
+        } else {
+            foreach ($product->productPages as $lang => $pages) {
+                $string .= '$products[\'' . $uid . '\'][\'productPages\'][\'' . $lang . '\'] = array(';
+                if (is_array($pages)) {
+                    foreach ($pages as $page) {
+                        $string .= "'" . $this->cleanString($page) . "', ";
+                    }
+                }
+                $string .= ');' . "\n";
+            }
+        }
+        return $string;
+    }
+
+    private function cleanString($string)
+    {
+        $string = str_replace(array('./', '<?php', '<?', '?>'), '', $string);
         return addcslashes($string, '\'\\');
     }
 }
--- tests/unit/CatalogWriterTest.php
+++ tests/unit/CatalogWriterTest.php
@@ -8,6 +8,137 @@ use org\bovigo\vfs\vfsStreamDirectory;
 
 class CatalogWriterTest extends TestCase
 {
+    const PRODUCT_A = array(
+        'names' => ['de' => 'Kaffee (im Versandbecher)', 'en' => 'Coffee (in shipping cup)'],
+        'descriptions' => array(
+            'de' => '<p>Coffee. Mediocrem erroribus forensibus ea mel, mea omnes eligendi eu. Sit sint eros ne.</p>',
+            'en' => '<p>Mediocrem erroribus forensibus ea mel, mea omnes eligendi eu. Sit sint eros ne. Hinc possim ex eum, vel electram expetenda ea.</p>'
+        ),
+        'teasers' => array(
+            'de' => '<p>Coffee. Mediocrem erroribus forensibus ea mel, mea omnes eligendi eu. Sit sint eros ne.</p>',
+            'en' => ''
+        ),
+        'price' => 2.75,
+        'productPages' => array(
+            'de' => [],
+            'en' => []
+        ),
+        'previewPicture' => 'preview_coffee.jpg',
+        'vat' => 'full',
+        'separator' => '/',
+        'categories' => array(
+            'de' => ['Trinken'],
+            'en' => ['Beverages']
+        ),
+        'stock_on_hand' => 1,
+        'weight' => 0.3,
+        'variants' => array(
+            'de' => [],
+            'en' => []
+        ),
+        'uid' => 'p4bbf07812257f',
+        'sortIndex' => 4,
+        'image' => 'coffee.jpg',
+        'imageFolder' => './userfiles/images/shop/',
+        'previewFolder' => './userfiles/images/shop/'
+    );
+
+    const PRODUCT_B = array(
+        'names' => ['de' => 'Brötchen', 'en' => 'Bread Rolls'],
+        'descriptions' => array(
+            'de' => '<p>Posse nostrum ne est, amet hinc intellegam et his. Cum tota modus exerci in, verterem aliquyam ei vix, nec id autem reprimique.</p><p>Posse nostrum ne est, amet hinc intellegam et his. Cum tota modus exerci in, verterem aliquyam ei vix, nec id autem reprimique.</p>',
+            'en' => '<p>Posse nostrum ne est, amet hinc intellegam et his. Cum tota modus exerci in, verterem aliquyam ei vix, nec id autem reprimique.</p>'
+        ),      
+        'teasers' => ['de' => '<p>Ganz fantastische Brötchen!</p>'],
+        'price' => 0.75,
+        'productPages' => array('de' => [], 'en' => []),
+        'previewPicture' => 'preview_bread.jpg',
+        'vat' => 'reduced',
+        'separator' => '/',
+        'categories' => array(
+            'de' => ['Essen', '– Backwaren'],
+            'en' => ['Delicious things', '– fresh from the bakery']
+        ),
+        'stock_on_hand' => 1,
+        'weight' => 0.04,
+        'variants' => array(
+            'de' => ['Sesam', 'Mohn', 'Käse'],
+            'en' => []
+        ),
+        'uid' => 'p4bbf078122572',
+        'sortIndex' => 3,
+        'image' => 'bread.jpg',
+        'imageFolder' => './userfiles/images/shop/',
+        'previewFolder' => './userfiles/images/shop/'
+    );
+
+    const CATALOG = <<<'EOT'
+<?php 
+################### Catalog ###############################;
+$version = '1.0';
+
+################### Categories ###############################;
+$categories['en'][0] = 'cat1';
+$categories['en'][1] = 'cat2';
+$categories['de'][0] = 'Kat1';
+$categories['de'][1] = 'Kat2';
+$default_category['en'] = 'defcat';
+$default_category['de'] = 'DefKat';
+$category_for_the_left_overs['en'] = 'this and that';
+$category_for_the_left_overs['de'] = 'dies und das';
+
+
+################### Products ######################;
+$products['p4bbf07812257f']['names']['de'] = 'Kaffee (im Versandbecher)';
+$products['p4bbf07812257f']['names']['en'] = 'Coffee (in shipping cup)';
+$products['p4bbf07812257f']['price'] = 2.75;
+$products['p4bbf07812257f']['vat'] = 'full';
+$products['p4bbf07812257f']['sortIndex'] = 4;
+$products['p4bbf07812257f']['previewPicture'] = 'preview_coffee.jpg';
+$products['p4bbf07812257f']['image'] = 'coffee.jpg';
+$products['p4bbf07812257f']['weight'] = 0.30;
+$products['p4bbf07812257f']['stock_on_hand'] = 1;
+$products['p4bbf07812257f']['teasers']['de'] = '<p>Coffee. Mediocrem erroribus forensibus ea mel, mea omnes eligendi eu. Sit sint eros ne.</p>';
+$products['p4bbf07812257f']['teasers']['en'] = '';
+$products['p4bbf07812257f']['descriptions']['de'] = '<p>Coffee. Mediocrem erroribus forensibus ea mel, mea omnes eligendi eu. Sit sint eros ne.</p>';
+$products['p4bbf07812257f']['descriptions']['en'] = '<p>Mediocrem erroribus forensibus ea mel, mea omnes eligendi eu. Sit sint eros ne. Hinc possim ex eum, vel electram expetenda ea.</p>';
+$products['p4bbf07812257f']['variants']['de'] = array();
+$products['p4bbf07812257f']['variants']['en'] = array();
+$products['p4bbf07812257f']['categories']['de'] = array('Trinken', );
+$products['p4bbf07812257f']['categories']['en'] = array('Beverages', );
+$products['p4bbf07812257f']['productPages']['de'] = array();
+$products['p4bbf07812257f']['productPages']['en'] = array();
+$products['p4bbf07812257f']['separator'] = '/';
+$products['p4bbf07812257f']['uid'] = 'p4bbf07812257f';
+
+#-----------------------------------------------------
+
+$products['p4bbf078122572']['names']['de'] = 'Brötchen';
+$products['p4bbf078122572']['names']['en'] = 'Bread Rolls';
+$products['p4bbf078122572']['price'] = 0.75;
+$products['p4bbf078122572']['vat'] = 'reduced';
+$products['p4bbf078122572']['sortIndex'] = 3;
+$products['p4bbf078122572']['previewPicture'] = 'preview_bread.jpg';
+$products['p4bbf078122572']['image'] = 'bread.jpg';
+$products['p4bbf078122572']['weight'] = 0.04;
+$products['p4bbf078122572']['stock_on_hand'] = 1;
+$products['p4bbf078122572']['teasers']['de'] = '<p>Ganz fantastische Brötchen!</p>';
+$products['p4bbf078122572']['descriptions']['de'] = '<p>Posse nostrum ne est, amet hinc intellegam et his. Cum tota modus exerci in, verterem aliquyam ei vix, nec id autem reprimique.</p><p>Posse nostrum ne est, amet hinc intellegam et his. Cum tota modus exerci in, verterem aliquyam ei vix, nec id autem reprimique.</p>';
+$products['p4bbf078122572']['descriptions']['en'] = '<p>Posse nostrum ne est, amet hinc intellegam et his. Cum tota modus exerci in, verterem aliquyam ei vix, nec id autem reprimique.</p>';
+$products['p4bbf078122572']['variants']['de'] = array('Sesam', 'Mohn', 'Käse', );
+$products['p4bbf078122572']['variants']['en'] = array();
+$products['p4bbf078122572']['categories']['de'] = array('Essen', '– Backwaren', );
+$products['p4bbf078122572']['categories']['en'] = array('Delicious things', '– fresh from the bakery', );
+$products['p4bbf078122572']['productPages']['de'] = array();
+$products['p4bbf078122572']['productPages']['en'] = array();
+$products['p4bbf078122572']['separator'] = '/';
+$products['p4bbf078122572']['uid'] = 'p4bbf078122572';
+
+#-----------------------------------------------------
+
+?>
+EOT;
+
     private $root;
 
     protected function setUp()
@@ -27,9 +158,13 @@ class CatalogWriterTest extends TestCase
             ),
             'default_category' => ['en' => 'defcat', 'de' => 'DefKat'],
             'category_for_the_left_overs' => ['en' => 'this and that', 'de' => 'dies und das'],
-            'products' => []
+            'products' => array(
+                'p4bbf07812257f' => Product::createFromRecord(self::PRODUCT_A, 1, '/'),
+                'p4bbf078122572' => Product::createFromRecord(self::PRODUCT_B, 2, '/')
+            ),
         );
         $subject = new CatalogWriter($catalog);
         $subject->write();
+        $this->assertEquals(self::CATALOG, $this->root->getChild('catalog.php')->getContent());
     }
 }
