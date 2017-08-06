cmsimple-xh/xhshop

commit 6c50a35ed06d2fbbe2982b5ed0f96a236f5ef040

Extract method getConfirmationParameters()

classes/FrontEndController.php
@@ -357,6 +357,17 @@ class FrontEndController extends Controller
 
     private function htmlConfirmation()
     {
+        return $this->render('confirmation_email/html', $this->getConfirmationParameters());
+    }
+
+    private function textConfirmation()
+    {
+        return $this->render('confirmation_email/text', $this->getConfirmationParameters());
+    }
+
+    private function getConfirmationParameters()
+    {
+        $params = array();
         foreach ($_SESSION['xhsCustomer'] as $field => $value) {
             $params[$field]       = $value;
         }
@@ -380,32 +391,7 @@ class FrontEndController extends Controller
             $params['fullRate']    = $this->settings['vat_full'];
             $params['reducedRate'] = $this->settings['vat_reduced'];
         }
-        return $this->render('confirmation_email/html', $params);
-    }
-
-    private function textConfirmation()
-    {
-        foreach ($_SESSION['xhsCustomer'] as $field => $value) {
-            $params[$field]       = $value;
-        }
-        $params['fee']        = $this->calculatePaymentFee();
-        $params['cartItems']  = $this->collectCartItems();
-        $params['cartSum']    = $_SESSION['xhsOrder']->getCartSum();
-        $params['shipping']   = $_SESSION['xhsOrder']->getShipping();
-        $params['total']      = $_SESSION['xhsOrder']->getTotal();
-        $params['vatTotal']   = $_SESSION['xhsOrder']->getVat();
-        $params['vatFull']    = $_SESSION['xhsOrder']->getVatFull();
-        $params['vatReduced'] = $_SESSION['xhsOrder']->getVatReduced();
-        $params['contact_name']    = $this->settings['name'];
-        $params['payment']    = $this->paymentModules[$_SESSION['xhsCustomer']->payment_mode]->getLabelString();
-        if ($this->settings['dont_deal_with_taxes']) {
-            $params['hideVat'] = true;
-        } else {
-            $params['hideVat']     = false;
-            $params['fullRate']    = $this->settings['vat_full'];
-            $params['reducedRate'] = $this->settings['vat_reduced'];
-        }
-        return $this->render('confirmation_email/text', $params);
+        return $params;
     }
 
     private function finalConfirmation()

commit c172f3e08d48c043f31fc0385a6d641454197ae5

Fix regression introduced with 6c50a35
We must not call `nl2br()` for plain text emails.

classes/FrontEndController.php
@@ -360,31 +360,31 @@ class FrontEndController extends Controller
 
     private function htmlConfirmation()
     {
-        return $this->render('confirmation_email/html', $this->getConfirmationParameters());
+        return $this->render('confirmation_email/html', $this->getConfirmationParameters(true));
     }
 
     private function textConfirmation()
     {
-        return $this->render('confirmation_email/text', $this->getConfirmationParameters());
+        return $this->render('confirmation_email/text', $this->getConfirmationParameters(false));
     }
 
     private function additionalHtmlMail()
     {
-        return $this->render('additional_email/html', $this->getConfirmationParameters());
+        return $this->render('additional_email/html', $this->getConfirmationParameters(true));
     }
 
     private function additionalTextMail()
     {
-        return $this->render('additional_email/text', $this->getConfirmationParameters());
+        return $this->render('additional_email/text', $this->getConfirmationParameters(false));
     }
 
-    private function getConfirmationParameters()
+    private function getConfirmationParameters($html)
     {
         $params = array();
         foreach ($_SESSION['xhsCustomer'] as $field => $value) {
             $params[$field]       = $value;
         }
-        if (isset($params['annotation'])) {
+        if (isset($params['annotation']) && $html) {
             $params['annotation'] = nl2br($params['annotation']);
         }
         $params['fee']        = $this->calculatePaymentFee();