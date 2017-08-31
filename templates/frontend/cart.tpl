cmsimple-xh/xhshop

commit 5dd2060941f2f4162c6488d13370341a68445ced

Fix add to cart behavior
Adding goods to the cart should actually *add* them instead of
*replacing* them. For instance, adding a certain item twice, should
result in having two of these items in the cart. On the other hand,
adding a negative amount should remove this amount from the items in
the cart.

During the first step of the final checkout, the customer still can
change the amounts as before, i.e. the form works by replacing the
items here.

templates/frontend/cart.tpl
@@ -29,6 +29,7 @@ foreach($this->cartItems as $product){
 <!--<input class="xhsInpAmount" type="number" min="1" step="1" name="xhsAmount" value="<?php echo $product['amount']; ?>" onChange="this.parentNode.submit()">-->
 <input class="xhsInpAmount" type="number" min="1" step="1" name="xhsAmount" value="<?php echo $product['amount']; ?>"> x <?php echo $this->formatCurrency($product['price']) ;?><br>
 <input type="hidden" name="xhsTask" value="updateCart">
+<input type="hidden" name="xhsReplace" value="1">
 <input type="hidden" name="xhsVariant" value="<?php echo $product['variantKey']; ?>">
 <input type="hidden" name="cartItem" value="<?php echo $product['key'];?>">
 <button class="xhsUpdBtn" title="<?php $this->label('update'); ?>"><span class="fa fa-refresh fa-lg"></span></button>

classes/FrontEndController.php
@@ -92,10 +92,14 @@ class FrontEndController extends Controller
             $_SESSION['xhsOrder'] = new Order($this->settings['vat_full'], $this->settings['vat_reduced']);
         }
 
-        if ((int) $_POST['xhsAmount'] > 0) {
-            $_SESSION['xhsOrder']->addItem($this->catalog->getProduct($_POST['cartItem']), $_POST['xhsAmount'], $variant);
+        if (isset($_POST['xhsReplace'])) {
+            if ((int) $_POST['xhsAmount'] > 0) {
+                $_SESSION['xhsOrder']->addItem($this->catalog->getProduct($_POST['cartItem']), $_POST['xhsAmount'], $variant);
+            } else {
+                $_SESSION['xhsOrder']->removeItem($this->catalog->getProduct($_POST['cartItem']), $variant);
+            }
         } else {
-            $_SESSION['xhsOrder']->removeItem($this->catalog->getProduct($_POST['cartItem']), $variant);
+            $_SESSION['xhsOrder']->addItem($this->catalog->getProduct($_POST['cartItem']), $_POST['xhsAmount'], $variant, false);
         }
         $_SESSION['xhsOrder']->setShipping($this->calculateShipping());
 

classes/Order.php
@@ -69,13 +69,21 @@ class Order
         $this->fee = Decimal::zero();
     }
 
-    public function addItem(Product $product, $amount, $variant = null)
+    public function addItem(Product $product, $amount, $variant = null, $replace = true)
     {
         $index = $product->getUid();
         if (isset($variant)) {
             $index .= '_'.$variant;
         }
-        $this->items[$index]['amount'] = (int)$amount;
+        if ($replace) {
+            $this->items[$index]['amount'] = (int)$amount;
+        } else {
+            $this->items[$index]['amount'] += (int)$amount;
+            if ($this->items[$index]['amount'] <= 0) {
+                $this->removeItem($product, $variant);
+                return;
+            }
+        }
         $this->items[$index]['variant'] = $variant;
         $this->items[$index]['gross'] = $product->getGross();
         $this->items[$index]['vatRate'] = $product->getVat();

commit 0fec418fc9933a165058d4a810c72ba13c311fea

Fix #182: Delete button in cart doesn't work anymore
This regression has been introduced in 5dd2060, where we forgot to add
the `xhsReplace` parameter to the delete product form.

templates/frontend/cart.tpl
@@ -37,6 +37,7 @@ foreach($this->cartItems as $product){
 %CSRF_TOKEN_INPUT%
 <input type="hidden" name="xhsAmount" value="0">
 <input type="hidden" name="xhsTask" value="updateCart">
+<input type="hidden" name="xhsReplace" value="1">
 <input type="hidden" name="xhsVariant" value="<?php echo $product['variantKey']; ?>">
 <input type="hidden" name="cartItem" value="<?php echo $product['key'];?>">
 <button class="xhsDelBtn" title="<?php $this->label('delete'); ?>"><span class="fa fa-remove fa-lg"></span></button>